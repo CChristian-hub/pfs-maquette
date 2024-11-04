f57a2ca1cb559b73176d3946bcbeb6fe2dd77968
C.Christian <christian.chang.pro@gmail.com>
2024-11-04 23:19:29 +0100

    Add: Pictures in public folder, left part of the product page, not done yet

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,21 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/style.css">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:wght@300;400;500&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css">
     <title>Product Page</title>
 </head>
 
 <body>
     <div class="navigation-history">
         {{-- change this into a component --}}
-        <a href="#">Home</a> >
-        <a href="#">Women</a> >
+        <a href="#">Home</a> &#8250;
+        <a href="#">Women</a> &#8250;
         <a href="#">Dresse</a>
     </div>
-    <div class="article-container">
-        <div class="article-picture"></div>
-        <div class="article-info"></div>
+    <div class="product-container">
+        <div class="product-picture">
+            <ul class="product-preview">
+                <li><img src="/images/picture-preview-01.jpeg" alt="mini-preview" class="image-small"></li>
+                <li><img src="/images/picture-preview-02.jpeg" alt="mini-preview" class="image-small"></li>
+                <li><img src="/images/picture-preview-01.jpeg" alt="mini-preview" class="image-small"></li>
+                <li><img src="/images/picture-preview-02.jpeg" alt="mini-preview" class="image-small"></li>
+                <li><img src="/images/picture-preview-03.jpeg" alt="mini-preview" class="image-small"></li>
+                <li><img src="/images/picture-preview-04.jpeg" alt="mini-preview" class="image-small"></li>
+            </ul>
+            <img class="image-big" src="./images/picture-preview-03.jpeg" alt="main-disp">
+            <p class="socials">Share :</p>
+        </div>
+        <div class="product-info">Desc</div>
     </div>
+
 </body>
 
 </html>
