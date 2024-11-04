11ab01e0211733b2e7653fbe9a4c6d556afb4ba5
C.Christian <christian.chang.pro@gmail.com>
2024-11-04 19:41:11 +0100

    Add: css file, editing page names and default route

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Product Page</title>
+</head>
+
+<body>
+    <div class="navigation-history">
+        {{-- change this into a component --}}
+        <a href="#">Home</a> >
+        <a href="#">Women</a> >
+        <a href="#">Dresse</a>
+    </div>
+    <div class="article-container">
+        <div class="article-picture"></div>
+        <div class="article-info"></div>
+    </div>
+</body>
+
+</html>
